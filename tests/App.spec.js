96f9bde0960609ebe29cbc303e56e0efae56c8c2
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2025-08-23 17:15:34 +0300

    first commit

---
+++ tests/App.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require("@playwright/test");
+const {  email,  password } = require("../user.js");
+
+test("successlAuthorization", async ({ page }) => {
+  await page.goto("https://netology.ru");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+
+  await expect(page).locator("[/profile/8820392");
+  await expect(page).toHaveTitle("Моё обучение");
+  await page.screenshot({ path: "successScreenshot.png" });
+});
+
+test("NotSuccessAuthorization", async ({ page }) => {
+  await page.goto("https://netology.ru");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").fill("Qwerty12345!");
+  await page.getByTestId("login-submit-btn").click();
+
+  await expect(page.getByTestId('login-error-hint')).toHaveText('Вы ввели неправильно логин или пароль');
+  await page.screenshot({ path: "NotsuccessScreenshot.png" });
+});
